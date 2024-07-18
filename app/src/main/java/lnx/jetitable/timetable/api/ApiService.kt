5ed09f7c41ad4bef2131513f4e2bfebc662823b8
thisdialynx <thisdialynx@outlook.com>
2024-07-18 17:44:41 +0300

    Rename service

--- app/src/main/java/lnx/jetitable/timetable/api/ApiService.kt
+++ app/src/main/java/lnx/jetitable/timetable/api/ApiService.kt
@@ -1,15 +1,21 @@
-package lnx.jetitable.timetable.api.login
+package lnx.jetitable.timetable.api
 
 import lnx.jetitable.timetable.api.login.data.LoginRequest
 import lnx.jetitable.timetable.api.login.data.LoginResponse
+import lnx.jetitable.timetable.api.login.data.MailRequest
+import lnx.jetitable.timetable.api.login.data.MailResponse
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
-interface AuthService {
-    @POST("/php/Avtorization.php")
+interface ApiService {
+    @POST("php/Avtorization.php")
     suspend fun checkPassword(
         @Header("Authorization") token: String,
         @Body request: LoginRequest
     ): LoginResponse
+    @POST("php/Avtorization.php")
+    suspend fun sendMail(
+        @Body request: MailRequest
+    ): MailResponse
 }
