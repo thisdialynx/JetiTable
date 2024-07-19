ed763fae37812ba4c9f356caa172ed6d9a0405e2
thisdialynx <thisdialynx@outlook.com>
2024-07-19 21:12:33 +0300

    Fix flashy transitions in dark theme

--- app/src/main/java/lnx/jetitable/activity/MainActivity.kt
+++ app/src/main/java/lnx/jetitable/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
@@ -27,7 +28,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetiTableTheme {
-                AppNavigation()
+                Surface {
+                    AppNavigation()
+                }
             }
         }
     }
