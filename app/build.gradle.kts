e603879b9ec554f379caa1964b4361d13051f69e
thisdialynx <thisdialynx@outlook.com>
2024-08-23 20:28:28 +0300

    Basic dark/light splash screen implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.retrofit2.converter.scalars)
     implementation(libs.jsoup.jsoup)
