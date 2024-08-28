02dff66d2702d4894fdef83f497c6725736088c5
thisdialynx <thisdialynx@outlook.com>
2024-08-28 23:55:06 +0300

    Working date picker (needs some improvements)

--- app/src/main/java/lnx/jetitable/screens/home/HomeViewModel.kt
+++ app/src/main/java/lnx/jetitable/screens/home/HomeViewModel.kt
@@ -12,25 +12,26 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import lnx.jetitable.BuildConfig
 import lnx.jetitable.datastore.UserDataManager
-import lnx.jetitable.timetable.api.dailyLessonListDataExtractor
 import lnx.jetitable.misc.getAcademicYear
-import lnx.jetitable.misc.getCurrentDate
 import lnx.jetitable.misc.getSemester
 import lnx.jetitable.timetable.api.ApiService.Companion.CHECK_ZOOM
 import lnx.jetitable.timetable.api.ApiService.Companion.DAILY_LESSON_LIST
 import lnx.jetitable.timetable.api.ApiService.Companion.STATE
 import lnx.jetitable.timetable.api.RetrofitHolder
+import lnx.jetitable.timetable.api.dailyLessonListDataExtractor
 import lnx.jetitable.timetable.api.login.data.User
 import lnx.jetitable.timetable.api.query.data.DailyLessonListRequest
 import lnx.jetitable.timetable.api.query.data.DailyLessonListResponse
 import lnx.jetitable.timetable.api.query.data.Lesson
 import lnx.jetitable.timetable.api.query.data.VerifyPresenceRequest
+import java.util.Locale
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
     private val context
         get() = getApplication<Application>().applicationContext
     private val userDataStore = UserDataManager(context)
     private val service = RetrofitHolder.getInstance(context)
+    private val dateFormat = "%02d.%02d.%d"
 
     var fullName by mutableStateOf<String?>(null)
         private set
@@ -48,15 +49,30 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
             fullName = user.fio
             group = user.group
             groupId = user.id_group
+
+            val calendar = Calendar.getInstance()
+            val currentDate = String.format(
+                Locale.getDefault(),
+                dateFormat,
+                calendar.get(Calendar.DAY_OF_MONTH),
+                calendar.get(Calendar.MONTH) + 1,
+                calendar.get(Calendar.YEAR)
+            )
+
+            getDailyLessonList(currentDate)
         }
     }
 
-    fun getDailyLessonList() {
+    fun onDateSelected(year: Int, month: Int, day: Int) {
+        val locale = Locale.getDefault()
+        val selectedDate = String.format(locale, dateFormat, day, month + 1, year)
+        getDailyLessonList(selectedDate)
+    }
+
+    private fun getDailyLessonList(date: String) {
         viewModelScope.launch {
             try {
                 val calendar = Calendar.getInstance()
-                val currentDate = getCurrentDate()
-
                 val currentYear = getAcademicYear(
                     calendar.get(Calendar.YEAR),
                     calendar.get(Calendar.MONTH) + 1
@@ -69,7 +85,7 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
                         DAILY_LESSON_LIST,
                         group,
                         firstGroupId,
-                        currentDate,
+                        date,
                         currentYear,
                         currentSemester
                     )
