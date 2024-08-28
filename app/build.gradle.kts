0b93b11133e238773e52265b37d4b530370bf06d
thisdialynx <thisdialynx@outlook.com>
2024-08-29 00:00:27 +0300

    0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "lnx.jetitable"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.1-alpha"
+        versionCode = 4
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
