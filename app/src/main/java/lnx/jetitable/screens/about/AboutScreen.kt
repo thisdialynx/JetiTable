f2248ed3425a584d43558ce858ceed4eb6073d3d
thisdialynx <thisdialynx@outlook.com>
2024-09-01 23:08:54 +0300

    Contributors column redesign

--- app/src/main/java/lnx/jetitable/screens/about/AboutScreen.kt
+++ app/src/main/java/lnx/jetitable/screens/about/AboutScreen.kt
@@ -1,11 +1,11 @@
 package lnx.jetitable.screens.about
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
@@ -37,11 +38,7 @@ fun AboutScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             LargeTopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(id = R.string.about_screen_title)
-                    )
-                },
+                title = { Text(text = stringResource(id = R.string.about_screen_title)) },
                 navigationIcon = {
                     IconButton(onClick = { navController.navigate(Home.route) }) {
                         Icon(
@@ -49,16 +46,15 @@ fun AboutScreen(navController: NavHostController) {
                             contentDescription = "Back"
                         )
                     }
-                },
+                }
             )
         }
-    ) { paddingValues ->
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
-                .padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
         ) {
             val localUriHandler = LocalUriHandler.current
 
@@ -69,7 +65,7 @@ fun AboutScreen(navController: NavHostController) {
                     .wrapContentHeight()
             ) {
                 AppInfo()
-                Spacer(modifier = Modifier.padding(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
                 Card(
                     modifier = Modifier.wrapContentWidth(),
                     colors = CardDefaults.cardColors(
@@ -97,31 +93,48 @@ fun AboutScreen(navController: NavHostController) {
                     }
                 }
             }
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                verticalArrangement = Arrangement.spacedBy(12.dp)
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = "Product",
+                modifier = Modifier.padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp)
+                )
             ) {
-                Spacer(modifier = Modifier.padding(4.dp))
-                ContributorCard(
-                    profilePicture = 0,
-                    title = "Dialynx",
-                    description = R.string.developer,
-                    icon = lnx.jetitable.ui.icons.Telegram,
-                    iconDescription = "Telegram",
-                    localUriHandler = localUriHandler,
-                    shortUri = "t.me/placeholder"
-                )
-                ContributorCard(
-                    profilePicture = 0,
-                    title = "Denys Ratov",
-                    description = R.string.timetable_developer,
-                    icon = lnx.jetitable.ui.icons.Telegram,
-                    iconDescription = "Telegram",
-                    localUriHandler = localUriHandler,
-                    shortUri = "t.me/placeholder"
-                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                ) {
+                    ContributorCard(
+                        profilePicture = 0,
+                        title = "Dialynx",
+                        description = R.string.developer,
+                        icon = lnx.jetitable.ui.icons.Telegram,
+                        iconDescription = "Telegram",
+                        localUriHandler = localUriHandler,
+                        shortUri = "t.me/placeholder"
+                    )
+                    HorizontalDivider(
+                        thickness = 2.dp,
+                        color = MaterialTheme.colorScheme.surface
+                    )
+                    ContributorCard(
+                        profilePicture = 0,
+                        title = "Denys Ratov",
+                        description = R.string.timetable_developer,
+                        icon = lnx.jetitable.ui.icons.Telegram,
+                        iconDescription = "Telegram",
+                        localUriHandler = localUriHandler,
+                        shortUri = "t.me/placeholder"
+                    )
+                }
             }
         }
     }
