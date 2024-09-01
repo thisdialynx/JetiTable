8c52c95ba6a15408307815d8c8faeadf00071e45
thisdialynx <thisdialynx@outlook.com>
2024-09-01 23:14:47 +0300

    Date shift buttons

--- app/src/main/java/lnx/jetitable/misc/CalendarUtils.kt
+++ app/src/main/java/lnx/jetitable/misc/CalendarUtils.kt
@@ -1,15 +1,5 @@
 package lnx.jetitable.misc
 
-import android.icu.util.Calendar
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-fun getCurrentDate(): String {
-    val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
-    val calendar = Calendar.getInstance()
-    return dateFormat.format(calendar.time)
-}
-
 fun getAcademicYear(year: Int, month: Int): String {
     return if (month >= 8) {
         "${year}/${year + 1}"
