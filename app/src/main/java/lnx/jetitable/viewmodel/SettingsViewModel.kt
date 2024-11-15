40d20a8a7808cb7fec597be3489e5da74fb8b3e2
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2024-11-16 00:46:58 +0200

    Use refactored variable names, collect userdata flow

--- app/src/main/java/lnx/jetitable/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/lnx/jetitable/viewmodel/SettingsViewModel.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import lnx.jetitable.datastore.CookieDataStore
 import lnx.jetitable.datastore.UserDataStore
@@ -37,13 +38,13 @@ class SettingsViewModel(application: Application): AndroidViewModel(application)
 
     init {
         viewModelScope.launch {
-            val user = userDataStore.getApiUserData()
-            userId = user.id_user
-            fullName = user.fio
-            fullNameId = user.id_fio
+            val user = userDataStore.getUserData().first()
+            userId = user.userId
+            fullName = user.fullName
+            fullNameId = user.fullNameId
             group = user.group
-            groupId = user.id_group
-            isFullTime = user.denne == 1
+            groupId = user.groupId
+            isFullTime = user.isFullTime == 1
             academicYear = getAcademicYear(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH + 1))
         }
     }
