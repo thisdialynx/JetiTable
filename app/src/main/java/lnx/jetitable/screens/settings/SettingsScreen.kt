bd52839d1ba8c5e62f97afe36638beb5d7489876
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-01-02 23:16:24 +0200

    Navigate to info screen from settings screen
    * Small font adjustments

--- app/src/main/java/lnx/jetitable/screens/settings/SettingsScreen.kt
+++ app/src/main/java/lnx/jetitable/screens/settings/SettingsScreen.kt
@@ -1,18 +1,26 @@
 package lnx.jetitable.screens.settings
 
 import android.icu.util.Calendar
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -21,8 +29,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import lnx.jetitable.R
 import lnx.jetitable.misc.getSemester
+import lnx.jetitable.navigation.About
 import lnx.jetitable.navigation.Home
 import lnx.jetitable.viewmodel.SettingsViewModel
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,9 +66,7 @@ fun SettingsScreen(navController: NavHostController) {
         val academicYear = settingsViewModel.academicYear
 
         Column(
-            modifier = Modifier
-                .padding(paddingValues)
-                .padding(horizontal = 16.dp),
+            modifier = Modifier.padding(paddingValues),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             TTAccountCard(
@@ -69,7 +77,46 @@ fun SettingsScreen(navController: NavHostController) {
                 toastText = "UID: $userId, GID: $groupId, FNID: $fullNameId.",
                 settingsViewModel = settingsViewModel
             )
+            Spacer(modifier = Modifier.height(2.dp))
+            SettingsList(navController)
         }
     }
 }
 
+@Composable
+fun SettingsList(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { navController.navigate(About.route) },
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.padding(horizontal = 32.dp, vertical = 16.dp)
+            ) {
+                Icon(
+                    imageVector = lnx.jetitable.ui.icons.google.Info,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary
+                )
+                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                    Text(
+                        text = "Info",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = "App version, links and contributors",
+                        style = MaterialTheme.typography.bodySmall,
+                    )
+                }
+            }
+
+        }
+    }
+}
