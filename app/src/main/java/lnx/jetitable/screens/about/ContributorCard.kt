0992508ec4a0311c4943cf589909af13f5e3a89e
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-02-02 13:37:10 +0200

    Change color scheme for some components
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/src/main/java/lnx/jetitable/screens/about/ContributorCard.kt
+++ app/src/main/java/lnx/jetitable/screens/about/ContributorCard.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -59,7 +58,7 @@ fun ContributorCard(profilePicture: Int, title: String, description: Int, icon: 
         Card(
             modifier = Modifier.wrapContentWidth(),
             colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(10.dp)
+                containerColor = MaterialTheme.colorScheme.surfaceContainerHigh
             ),
             onClick = { localUriHandler.openUri("https://$shortUri") }
         ) {
