8ef14c3e6af2b4fecfa8b4db5181a9b0ba2e625a
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-02-19 19:17:21 +0200

    Refactor settings viewmodel
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/src/main/java/lnx/jetitable/screens/settings/SettingsScreen.kt
+++ app/src/main/java/lnx/jetitable/screens/settings/SettingsScreen.kt
@@ -2,35 +2,27 @@ package lnx.jetitable.screens.settings
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import lnx.jetitable.R
-import lnx.jetitable.misc.DateManager
 import lnx.jetitable.navigation.About
 import lnx.jetitable.viewmodel.SettingsViewModel
 
@@ -46,6 +38,9 @@ enum class Setting(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(navController: NavHostController) {
+    val settingsViewModel: SettingsViewModel = viewModel()
+    val userDataUiState by settingsViewModel.userDataUiState.collectAsStateWithLifecycle()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -53,7 +48,7 @@ fun SettingsScreen(navController: NavHostController) {
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
                             contentDescription = "Back arrow"
                         )
                     }
@@ -61,83 +56,15 @@ fun SettingsScreen(navController: NavHostController) {
             )
         }
     ) { paddingValues ->
-        val settingsViewModel: SettingsViewModel = viewModel()
-        val context = LocalContext.current
-        val dateManager = DateManager()
-
-        val userId = settingsViewModel.userId
-        val fullName = settingsViewModel.fullName
-        val fullNameId = settingsViewModel.fullNameId
-        val group = settingsViewModel.group
-        val groupId = settingsViewModel.groupId
-        val isFullTime = if (settingsViewModel.isFullTime == true) R.string.full_time else R.string.part_time
-        val semester = if (dateManager.getSemester() == 1) R.string.autumn_semester else R.string.spring_semester
-        val academicYear = settingsViewModel.academicYear
-        val status = settingsViewModel.status
-
         Column(
             modifier = Modifier.padding(paddingValues),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            TTAccountCard(
-                title = stringResource(id = R.string.timetable_account, stringResource(id = R.string.timetable)),
-                description = "$fullName ($group). \n${stringResource(id = R.string.status)}: $status. $academicYear. ${stringResource(id = semester)}. ${stringResource(id = isFullTime)}.",
-                icon = lnx.jetitable.ui.icons.Snu,
-                context = context,
-                toastText = "UID: $userId, GID: $groupId, FNID: $fullNameId.",
-                settingsViewModel = settingsViewModel
-            )
+            AccountCard(
+                userDataUiState = userDataUiState
+            ) { settingsViewModel.signOut() }
             Spacer(modifier = Modifier.height(2.dp))
             SettingEntries(navController)
-        }
-    }
-}
-
-@Composable
-fun SettingEntries(navController: NavHostController) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-    ) {
-        Setting.entries.forEachIndexed { index, settingEntry ->
-            if (index > 0) {
-                HorizontalDivider(thickness = 2.dp, color = MaterialTheme.colorScheme.surface)
-            }
-
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-                onClick = { navController.navigate(settingEntry.destination) },
-                colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceContainer
-                )
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
-                    Icon(
-                        imageVector = lnx.jetitable.ui.icons.google.Info,
-                        contentDescription = "${stringResource(id = settingEntry.titleResId)}. ${stringResource(id = settingEntry.descriptionResId)}",
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-                    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                        Text(
-                            text = stringResource(id = settingEntry.titleResId),
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                        Text(
-                            text = stringResource(id = settingEntry.descriptionResId),
-                            style = MaterialTheme.typography.bodySmall,
-                        )
-                    }
-                }
-            }
         }
     }
 }
