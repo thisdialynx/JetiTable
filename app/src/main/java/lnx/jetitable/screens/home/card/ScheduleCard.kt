ed1d0255664b38121edc9945cc4a76d06b394f3c
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-02-19 23:32:35 +0200

    Re-setup content animations
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/src/main/java/lnx/jetitable/screens/home/card/ScheduleCard.kt
+++ app/src/main/java/lnx/jetitable/screens/home/card/ScheduleCard.kt
@@ -1,25 +1,21 @@
 package lnx.jetitable.screens.home.card
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateContentSize
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 @Composable
 fun ScheduleCard(
     expanded: Boolean = true,
-    modifier: Modifier = Modifier,
     title: @Composable () -> Unit,
     content: @Composable () -> Unit
 ) {
     Card(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceContainer
-        ),
-        modifier = modifier.animateContentSize()
+        )
     ) {
         title()
         AnimatedVisibility(
