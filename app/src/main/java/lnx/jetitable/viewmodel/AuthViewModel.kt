3d4420edb7294f8c5689bdbe985eb49f4fb1de94
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-02-19 16:39:13 +0200

    Add AuthState to manage different states
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/src/main/java/lnx/jetitable/viewmodel/AuthViewModel.kt
+++ app/src/main/java/lnx/jetitable/viewmodel/AuthViewModel.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
@@ -18,56 +17,61 @@ import lnx.jetitable.timetable.api.login.data.LoginRequest
 import lnx.jetitable.timetable.api.login.data.MailRequest
 import okhttp3.Credentials
 
+sealed class AuthState {
+    object Authorized : AuthState()
+    data class Error(val messageResIs: Int) : AuthState()
+    object Idle : AuthState()
+}
+
 class AuthViewModel(application: Application) : AndroidViewModel(application) {
     private val context
         get() = getApplication<Application>().applicationContext
     private val service = RetrofitHolder.getInstance(context)
 
-    var isAuthorized by mutableStateOf(false)
+    var authState by mutableStateOf<AuthState>(AuthState.Idle)
         private set
     var password by mutableStateOf("")
         private set
     var email by mutableStateOf("")
         private set
-    var errorMessage by mutableIntStateOf(0)
-        private set
 
     fun updatePassword(value: String) { password = value }
     fun updateEmail(value: String) { email = value }
-    fun clearErrorMessage() { errorMessage = 0 }
+    fun clearErrorMessage() { authState = AuthState.Idle }
+
     private fun checkEmail(login: String): Boolean {
         return if (!login.endsWith("@snu.edu.ua")) {
-            errorMessage = R.string.corporate_email_error
+            authState = AuthState.Error(R.string.corporate_email_error)
             true
-        } else { false }
+        } else false
     }
 
     fun checkCredentials() {
         viewModelScope.launch {
             try {
+                if (checkEmail(email)) return@launch
+
                 val basicAuth = Credentials.basic(email, password)
-
-                if (checkEmail(email)) {
-                    errorMessage = R.string.corporate_email_error
+                val response = service.checkPassword(
+                    basicAuth,
+                    LoginRequest(CHECK_PASSWORD, email, password)
+                )
+                if (response.status == "ok") {
+                    authState = AuthState.Authorized
                 } else {
-                    val response = service.checkPassword(
-                        basicAuth,
-                        LoginRequest(CHECK_PASSWORD, email, password)
-                    )
-
-                    if (response.status == "ok") {
-                        isAuthorized = true
-                    } else { errorMessage = R.string.wrong_credentials }
+                    authState = AuthState.Error(R.string.wrong_credentials)
                 }
+
             } catch (e: Exception) {
-                Log.e("TimeTable authentication", "Login process error.\nisAuthorized: $isAuthorized", e)
+                Log.e("TimeTable authentication", "Login process error.\nisAuthorized: $authState", e)
             }
         }
 
     }
 
     fun sendMail() {
-        checkEmail(email)
+        if (checkEmail(email)) return
+
         viewModelScope.launch {
             try {
                 val response = service.sendMail(MailRequest(SEND_MAIL, email))
