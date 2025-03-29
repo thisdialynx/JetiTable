95b1f4e03d559f1b68b1788c99219988abbc598c
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-03-29 16:19:16 +0200

    0.1.8
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "lnx.jetitable"
         minSdk = 24
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.1.7"
+        versionCode = 22
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
