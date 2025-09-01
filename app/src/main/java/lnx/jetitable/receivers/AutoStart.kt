6a53aff3ae34238df975563b92a3624b56127db7
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-09-01 18:11:46 +0300

    Do not start the service with also block
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/src/main/java/lnx/jetitable/receivers/AutoStart.kt
+++ app/src/main/java/lnx/jetitable/receivers/AutoStart.kt
@@ -7,10 +7,10 @@ import lnx.jetitable.services.data.DataSyncService
 
 class AutoStart: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == Intent.ACTION_BOOT_COMPLETED && context != null) {
-            Intent(context, DataSyncService::class.java).also {
-                context.startService(it)
-            }
-        }
+        if (context == null) return
+        if (intent?.action != Intent.ACTION_BOOT_COMPLETED) return
+
+        val serviceIntent = Intent(context, DataSyncService::class.java)
+        context.startService(serviceIntent)
     }
 }
