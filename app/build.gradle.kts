bac02a527bec571c5610fd0f169f8a8097aaafa7
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-09-26 12:33:41 +0300

    build: compose: remove extension version declaration
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,9 +53,7 @@ android {
         compose = true
         buildConfig = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
