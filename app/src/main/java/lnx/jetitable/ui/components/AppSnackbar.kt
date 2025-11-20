7e83bb9488a232a3720cdebc0e9fe98939f8ecae
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2025-11-21 00:08:50 +0200

    ui: use surfaceCardAtElevation instead
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/src/main/java/lnx/jetitable/ui/components/AppSnackbar.kt
+++ app/src/main/java/lnx/jetitable/ui/components/AppSnackbar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,7 +16,7 @@ fun AppSnackbar(
 ) {
     Snackbar(
         modifier = modifier,
-        containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+        containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp),
         contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
         actionColor = MaterialTheme.colorScheme.primary,
         dismissActionContentColor = MaterialTheme.colorScheme.secondary,
