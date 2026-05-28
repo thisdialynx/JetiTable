50de5c149912f5370193b315e88240a0f0fdd31a
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2026-05-28 21:58:06 +0300

    sync: set constraints and back off criteria for sync request
    - sync only when battery precent is not low and has internet connection
    - retry syncing the data linearly
    
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/src/main/java/lnx/jetitable/services/data/DataSyncService.kt
+++ app/src/main/java/lnx/jetitable/services/data/DataSyncService.kt
@@ -3,9 +3,13 @@ package lnx.jetitable.services.data
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.work.BackoffPolicy
+import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import androidx.work.WorkRequest
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -18,7 +22,18 @@ class DataSyncService @Inject constructor() : Service() {
     override fun onBind(intent: Intent?): IBinder? = null
 
     fun scheduleDataSync() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .build()
+
         val syncRequest = PeriodicWorkRequestBuilder<UserDataWorker>(6, TimeUnit.HOURS)
+            .setConstraints(constraints)
+            .setBackoffCriteria(
+                BackoffPolicy.LINEAR,
+                WorkRequest.DEFAULT_BACKOFF_DELAY_MILLIS,
+                TimeUnit.MILLISECONDS
+            )
             .addTag(DATA_SYNC_SERVICE_NAME)
             .build()
 
