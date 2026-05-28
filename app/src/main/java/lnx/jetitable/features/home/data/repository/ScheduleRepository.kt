555d7ee61dc406314d82e6772424d42e2322075b
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2026-05-28 21:06:57 +0300

    repo(schedule): exams: don't check for date on saving
    - fixes saved exam list showing on no internet
    
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- app/src/main/java/lnx/jetitable/features/home/data/repository/ScheduleRepository.kt
+++ app/src/main/java/lnx/jetitable/features/home/data/repository/ScheduleRepository.kt
@@ -109,9 +109,7 @@ class ScheduleRepositoryImpl @Inject constructor(
             if (response.isSuccessful) {
                 when (val body = response.body()) {
                     is HtmlConverterState.Success -> {
-                        if (currentDay == dateHelper.selectedDate) dataStore.saveExamScheduleList(
-                            body.data
-                        )
+                        dataStore.saveExamScheduleList(body.data)
                         Success(body.data)
                     }
 
@@ -131,6 +129,8 @@ class ScheduleRepositoryImpl @Inject constructor(
             }
         } catch (e: IOException) {
             val cache = dataStore.getExamList().first()
+
+            Log.d("exam cache", cache.toString())
 
             if (cache.isNotEmpty()) {
                 Success(cache)
